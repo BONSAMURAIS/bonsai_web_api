45c56303da024a8f5290fe759549913ab759384d
Romain Sacchi <romain.sacchi@aalborgportland.com>
2019-03-25 21:24:24 +0100

    Added Flask project and updated README

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='bonsai_web_api',
+    version='0.1dev',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
