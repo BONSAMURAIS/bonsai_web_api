1dabdaea17d159688768c69f305d6591125bc772
Romain Sacchi <romain.sacchi@aalborgportland.com>
2019-03-26 19:22:28 +0100

    Updated web_api.py

--- bonsai_web_api/auth.py
+++ bonsai_web_api/auth.py
@@ -77,7 +77,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('web_app.index'))
 
 
 def login_required(view):
